09ca357fcc92b47a8933b2cd923653d6ceb87db6
Sonu Rai <sonuraimr@gmail.com>
2023-11-26 21:03:32 +0530

    Delete database directory

--- database/datatypes.py
+++
@@ -1,8 +0,0 @@
-class DataType:
-    @classmethod
-    def all(cls):
-        return [getattr(cls, k) for k in dir(cls) if k.isupper()]
-
-class LinkPrecedenceTypes(DataType):
-    SECONDARY = 'secondary'
-    PRIMARY = 'primary'
